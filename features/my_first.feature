61c6d19bb395d09baf7190d28e82ecaf5a28565f
laithnurie <laithnurie@gmail.com>
2014-07-14 01:55:28 +0100

    new tests

--- features/my_first.feature
+++ features/my_first.feature
@@ -2,7 +2,14 @@ Feature: Login feature
 
   Scenario: I should see dashboard items
     Given I am on the main Dashboard
-    Then I press "Manga"
-    Then I should see "Naruto"
-    Then I should see "Bleach"
-    Then I should see "Naruto"
+    And I should see 2 buttons
+    And I press "Manga"
+    Then I am on manga activity
+    Then I should see 3 buttons
+
+  Scenario: I should see mangas
+    Given I am on the main Dashboard
+    And I press "Manga"
+    Then I am on manga activity
+    Then I press naruto button
+    Then I should see naruto mangas
--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -1,5 +1,32 @@
 Given(/^I am on the main Dashboard$/) do
-  %{ Then I should see "Manga1",
-     And I should see "Travel"
+
+  steps %Q{
+    Then I should see "Manga"
+    And I should see "Travel"
+    Then I should see 2 buttons
   }
+
+end
+
+Then(/^I should see (\d+) buttons$/) do |no_of_buttons|
+  buttons_on_dashboard = query('Button').length
+  if buttons_on_dashboard.equal? no_of_buttons.to_i
+    puts "number of buttons #{query('Button').length}"
+  else
+    fail
+  end
+
+end
+
+Then(/^I am on manga activity$/) do
+  wait_for(:timeout => 5) { element_exists("Button id:'narutoBtn'") }
+end
+
+Then(/^I press (.*) button$/) do |manga_item|
+  touch("Button id:'#{manga_item}Btn'")
+end
+
+Then(/^I should see naruto mangas$/) do
+  wait_for(:timeout => 5) { element_exists("Textview id:'chapter'") }
+  true if query("Textview id:'chapter'").length > 3
 end
